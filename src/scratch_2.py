10f15e5554ef95ee5f5bc52b04479a3ecb311c7b
unknown <chhsh@yahoo.com>
2019-08-24 21:29:37 -0400

    Remove unused files

--- src/scratch_2.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-image = cv2.imread('C:\\apps\\opencv\\sources\\samples\\data\\images\\frame0.jpg')
-print;
-type(image)
-
-cv2.imshow('Test image', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
